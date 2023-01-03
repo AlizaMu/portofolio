3a4492963f395dedb9c921ec86231cdb6d62028f
AlizaMu <111516346+AlizaMu@users.noreply.github.com>
2023-01-03 23:27:41 +0700

    Add files via upload

---
+++ segitiga.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.write("""
+# Aplikasi Luas Lingkaran
+Ini adalah aplikasi menghitung luas lingkaran sederhana menggunakan Streamlit
+""")
+
+jari_jari = st.number_input("Masukkan Jari-jari", 0)
+
+hitung = st.button("Hitung Luas")
+
+if hitung:
+    luas = pi * jari_jari * jari_jari
+    st.success(f"Luas Lingkarannya adalah {luas}")
+    st.ballons()
